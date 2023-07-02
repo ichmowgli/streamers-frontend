d2bb9ec318d31ddb335c9464d7bf2f4f702a1070
Mariia Kucheieva <mary.kucheeva@gmail.com>
2023-07-02 21:23:18 +0300

    fuck

--- src/env.mjs
+++ src/env.mjs
@@ -13,10 +13,10 @@ export const env = createEnv({
   },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    API_URL: process.env.NEXT_PUBLIC_API_URL,
-    WS_URL: process.env.NEXT_PUBLIC_WS_URL,
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL
+    API_URL: process.env.API_URL,
+    WS_URL: process.env.WS_URL,
+    NEXT_PUBLIC_API_URL: process.env.API_URL,
+    NEXT_PUBLIC_WS_URL: process.env.WS_URL
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 });
